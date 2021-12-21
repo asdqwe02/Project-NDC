3b0a5c2a490acf80cd5ca6eef0764fb59b431ebc
HuyRand <82890282+HuyRand@users.noreply.github.com>
2021-12-21 15:38:36 +0700

    Added Buff in h/o _1

--- Da Game/Assets/Controller/PortalHideoutController.cs
+++ Da Game/Assets/Controller/PortalHideoutController.cs
@@ -2,9 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 public class PortalHideoutController : Interactable
 {
-
+    public GameObject ToDisplay;
+    public TextMeshProUGUI TextToDisplay;
     public string sceneName;
     Collider2D colliderP;
     [SerializeField] private string newScenePassword;
@@ -14,7 +16,9 @@ public class PortalHideoutController : Interactable
         PlayerController.instance.Coin_tobeAdded = 0;
         PlayerController.instance.scenePassword = newScenePassword;
         PlayerController.instance.Save();
-        SceneManager.LoadScene(sceneName);
+        
+        Activate_Buffs();
+        //SceneManager.LoadScene(sceneName);
 
     }
     private void OnTriggerEnter2D(Collider2D collision)
@@ -25,5 +29,64 @@ public class PortalHideoutController : Interactable
     {
         colliderP = null;
         TurnOffIIcon(collision);
+    }
+
+    private void AddHP()
+    {
+        PlayerController.instance.Hp += (int)(PlayerController.instance.Hp * 0.1);
+        PlayerController.instance.MaxHP = PlayerController.instance.Hp;
+    }
+
+    private void AddMS()
+    {
+        PlayerController.instance.MovementSpeed += (float)(PlayerController.instance.MovementSpeed * 0.1);
+    }
+
+    private void AddAS()
+    {
+        PlayerController.instance.AttackSpeed += (float)(PlayerController.instance.AttackSpeed * 0.1);
+    }
+
+    private void AddArmour()
+    {
+        PlayerController.instance.Armour += (int)(PlayerController.instance.Armour * 0.1);
+    }
+
+
+    private void Activate_Buffs()
+    {
+        ToDisplay.SetActive(true); //activate UI
+        //Display reveiced buffs 
+        string Text_Display = "Buffs received as follow : \n";
+        for (int i =0; i< PlayerController.instance.UnlockedSlot;i++)
+        {
+            System.Random random = new System.Random();
+            int x = random.Next(1, 101);
+            if (x < 11) // Numbers 1..10 ( A -> 10% )
+            {
+                AddAS();
+                //Display reveiced buffs 
+                Text_Display += "+10% Attack Speed \n";
+            }
+            else if (x < 21) // Numbers 11..20 ( B -> 10 % )
+            {
+                AddMS();
+                //Display reveiced buffs 
+                Text_Display += "+10% Movement Speed \n";
+            }
+            else if (x < 41) // Numbers 21..40 ( C -> 20 % )
+            {
+                AddArmour();
+                //Display reveiced buffs 
+                Text_Display += "+10% Armour \n";
+            }
+            else if (x < 101) // Numbers 41..100 ( D -> 60 % ) 
+            {
+                AddHP();
+                //Display reveiced buffs 
+                Text_Display += "+10% HP  \n";
+            }
+        }
+
     }
 }
