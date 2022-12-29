08913dc770c23786439ed4ab6d66dd35d393eced
asdqwe02 <thien.nguyenminh@ncc.asia>
2022-12-29 21:53:59 +0700

    patch 1.2.8 update

--- Da Game/Assets/Controller/DungeonPortalController.cs
+++ Da Game/Assets/Controller/DungeonPortalController.cs
@@ -1,31 +1,41 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using Random = UnityEngine.Random;
 
 public class DungeonPortalController : Interactable
 {
     private string nextSceneName, nextScenePassword;
+
     public enum NextRoom
     {
         NormalRoom,
         BossRoom,
     }
+
     public NextRoom room;
+
     public struct NextRoomInfo
     {
         public string SceneName;
         public string ScenePassword;
+
         public NextRoomInfo(string sname, string spassword)
         {
             SceneName = sname;
             ScenePassword = spassword;
         }
     }
+
     NextRoomInfo[] BossRoomInfo;
-    private void Awake() {
-        BossRoomInfo = new NextRoomInfo[]{
-            new NextRoomInfo("Room_Boss1","Boss_Room1_Entrance"),
+
+    private void Awake()
+    {
+        BossRoomInfo = new NextRoomInfo[]
+        {
+            new NextRoomInfo("Room_Boss1", "Boss_Room1_Entrance"),
             new NextRoomInfo("Room_Boss2", "Boss_Room2_Entrance")
         };
         switch (room)
@@ -33,26 +43,35 @@ public class DungeonPortalController : Interactable
             case NextRoom.NormalRoom:
                 break;
             case NextRoom.BossRoom:
-                int index = Random.Range(0,BossRoomInfo.Length);
+                int index = Random.Range(0, BossRoomInfo.Length);
                 nextSceneName = BossRoomInfo[index].SceneName;
                 nextScenePassword = BossRoomInfo[index].ScenePassword;
                 break;
             default:
                 break;
         }
+        // dumb as fuck override here
+        if (SceneManager.GetActiveScene().name == "Room1")
+        {
+            nextSceneName = "Room2";
+            nextScenePassword = "Room_1_Entrance";
+        }
     }
-    public override void Interact(){
+
+    public override void Interact()
+    {
         PlayerController.instance.CloseInteractableIcon();
         PlayerController.instance.scenePassword = nextScenePassword;
         SceneManager.LoadScene(nextSceneName);
     }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         TurnOnIIcon(collision);
     }
+
     private void OnTriggerExit2D(Collider2D collision)
     {
         TurnOffIIcon(collision);
     }
-
 }
